f655a9f73e14d0405695fd61a673017a70189abe
LGNDH <david.hybs@protonmail.com>
2026-04-16 08:03:35 +0200

    calculator

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def main():
     #prompt from user, using argparse
     parser = argparse.ArgumentParser(description="AI_agent")
     parser.add_argument("Agent_request", type=str, help="Hello, please write your request.")
+    parser.add_argument("--verbose", action="store_true", help="Enable/Disable verbose output")
     args = parser.parse_args()
 
 
@@ -31,9 +32,12 @@ def main():
     #call generate content function
     response = generate_content(client, messages)
     
-    #printing answer and to terminal
-    print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
-    print("Response tokens: ", response.usage_metadata.candidates_token_count)
+    #printing answer to terminal and if verbose arg true aditional data
+    if args.verbose == True:
+        print("User prompt: ", args.Agent_request)
+        print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+        print("Response tokens: ", response.usage_metadata.candidates_token_count)
+
     print(response.text)
 
 
